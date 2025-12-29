67a89604725cb831e1fe71d18d3c0bfad9e5e1b1
pdrcorrea <pedrhc258@gmail.com>
2025-12-28 22:59:15 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "pontoview-vyre-v1";
+const ASSETS = [
+  "./",
+  "./painel.html",
+  "./sw.js",
+  "./data/news.json",
+  "./data/tips.json",
+  "./data/campaigns.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request).then((networkResp) => {
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, networkResp.clone()));
+        return networkResp;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
